91d202dafadf4803ab87c272bee3d0ff03b79769
Lexi White <lexicwhite@gmail.com>
2017-04-17 14:56:16 -0700

    LexisNexis output file splitting and renaming

---
+++ LexisNexisFileSplit.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+# LexisNexisFunctionsSplot.py takes a text file output of LexisNexis search
+# results, splits it into individual files for each article included
+# in the search output, numbers them, and saves them to a new folder.
+
+
+import re, os
+
+
+input_file = input("Enter file name: ")
+file_name = input("Enter base name for all of the files to be subset: ")
+
+new_folder = file_name + '_LexisNexis_Files'
+os.makedirs(new_folder)
+
+
+def lexinexis_filesplit(input_file, file_name):
+	"""
+	Takes LexisNexis Full-Text Document export and splits it into individual
+	files
+
+	"""
+	f_read = open(input_file)
+	count = 0
+	f_write = open(os.path.join(new_folder, file_name + str(count) + '.txt'), 'w')
+	for line in f_read:
+		if re.search('\d* of \d* DOCUMENT', line):
+			f_write.close()
+			count = count + 1
+			f_write = open(os.path.join(new_folder, file_name + "_" + str(count) + '.txt'), 'w')
+			f_write.write(line)
+
+		else:
+			f_write.write(line)
+
+			# Adds error message if numbered files are missing from the LexisNexis text file
+			if re.search('This document could not be formatted for delivery', line):
+				print('''There may be a missing document, please check
+				and make sure document ''' + str(count + 1) + ' is accounted for.')
+
+				# Full LexisNexis error message is:
+				# "This document could not be formatted for delivery. If your
+				# delivery request included other documents,
+				# they completed successfully."
+
+	f_write.close()
+	f_read.close()
+
+lexinexis_filesplit(input_file, file_name)
+print('Done.')
+
+#TODO: Add an option to zip files at the end
---
+++ LexisNexisRenameFiles.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+# LexisNexisRenameFiles.py - Takes LexisNexis files that are mis-numbered
+# after splitting with the LexisNexisFunctionsSplot.py script and adjusts the
+# numbers
+
+import shutil, os, re, send2trash
+
+# Create a new folder to store the renamed files
+os.makedirs('ReNumbered')
+
+# Create a regex that recognizes the file names
+lxnxfilePattern = re.compile(r"""^(.*?)			# some base name text
+							(\d+)$				# the file number
+							""", re.VERBOSE)
+
+# Loop over the LexisNexis files
+for old_file in os.listdir('.'):
+	lxnxpattern = lxnxfilePattern.search(old_file)
+
+	if lxnxpattern == None:
+		continue
+
+	# Pulls the base file name and adjusts the numbering
+	base_name = lxnxpattern.group(1)
+	new_number = str(int(lxnxpattern.group(2)) + 1)
+
+	# Forms the new file name
+	new_filename = base_name + new_number
+
+	# Get the full absolute, file paths
+	absWorkingDir = os.path.abspath('.')
+	old_file = os.path.join(absWorkingDir, old_file)
+	absRenumbered = os.path.join(absWorkingDir, 'Renumbered')
+	new_filename = os.path.join(absRenumbered, new_filename)
+
+	# Rename the files
+	print('Renaming "%s" to "%s"...' % (old_file, new_filename))
+	shutil.move(old_file, new_filename) #uncomment after testing
+
+print('Done.')
